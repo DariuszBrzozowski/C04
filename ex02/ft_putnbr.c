ae708b03f5d2bb42b9cf48f0183f74200faba163
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-20 06:49:13 +0200

    ft_putnbr

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -2,8 +2,7 @@
 
 void	ft_putnbr(int nb)
 {
-	int div;
-	int mod;
+	
 	char nb_char;
 
 	if (nb == -2147483648)
@@ -11,30 +10,19 @@ void	ft_putnbr(int nb)
 		write(1, "-2147483648", 11);
 		return; 
 	}
-
-	else if (nb < 0)
+	if (nb < 0)
 	{
 		write(1, "-", 1);
 		nb = - nb;
 	}
-
-	if (nb < 10)
-	{
-		nb_char = nb + '0';
-		write(1, &nb_char, 1);
-	}
-	else 
-	{
-		div = nb/10;
-		mod = nb%10;
-		ft_putnbr(div);
-		ft_putnbr(mod);
-	}
-
+	if (nb >= 10)
+		ft_putnbr(nb/10);
+	nb_char = nb%10 + '0';
+	write(1, &nb_char, 1);
 }
 
 int main(int argc, char *argv[])
 {
-	ft_putnbr(-1234);
+	ft_putnbr(-120000034);
 	return(0);
 }
